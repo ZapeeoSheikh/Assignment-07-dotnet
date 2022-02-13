fe3e072a189e45464394b517c5762e436b2ea025
ZapeeoSheikh <muhammadrmz871@gmail.com>
2022-02-14 01:36:38 +0500

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Calculator
+    {
+        public int Sum( int a, int b){
+            int add = a + b;
+            return add;
+        }
+        public double Sum( double a, double b, double c){
+            double add = a + b + c;
+            return add;
+         }
+    }
+}
+
+
+// Muhammad Rameez 
+// Assignment 07
+
+        /*
+         //Overloading
+    Class Calculator
+    Sum(int, int)
+    Sum(double,double)
+    and use them in Program.cs
+
+
+    //Overriding
+    Worker (parent class) : Work() General Work
+    Carpenter :  Work() Wood Work
+    Blacksmith : Work() Iron Work*/
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Calculator calculator = new Calculator() ;
+
+// Method Overloading 
+Console.WriteLine(calculator.Sum(89, 34));
+Console.WriteLine(calculator.Sum(9.2, 3.3, 12.3));
+// There is not overriding occurs because number of paremeter are diffrent 
+
+Blacksmith blacksmith = new Blacksmith();
+Carpenter carpenter = new Carpenter();
+Worker worker = new Worker();
+
+// Method Overriding 
+string value1 = worker.Work = "General work";
+string value2 = carpenter.Work = "Wood work";
+string value3 = blacksmith.Work = "Iron work";
+Console.WriteLine(value1);
+Console.WriteLine(value2);
+Console.WriteLine(value3);
